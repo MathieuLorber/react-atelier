9bfd45006b0e12131eeccd62ba2ebacdd03c6593
Mathieu Lorber <mathieu.lorber@gmail.com>
2016-08-03 00:20:52 +0200

    s1

---
+++ atelier-client/src/common/Menu.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router';
+import styles from './Menu.scss';
+import {i18n} from '../i18n';
+
+import CSSModules from 'react-css-modules';
+
+@CSSModules(styles)
+export default class Menu extends React.Component {
+
+    render() {
+        return <aside className="main-sidebar" styleName="menu">
+            <section className="sidebar">
+                <div styleName="title-bloc">
+                    <div styleName="title">{i18n('Menu.ApplicationTitle')}</div>
+                    <ul className="sidebar-menu">
+                        <li>
+                            <a>
+                                <i className="fa fa-home"></i> {i18n('Menu.Home')}
+                                <i className="fa fa-angle-right" styleName="arrow"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </aside>
+    }
+}
